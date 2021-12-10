47f590cf7c65fd3a146e7227fb6a577b00543415
armando-dev <ah25632@gmail.com>
2021-12-09 22:20:26 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'softura_practice',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
